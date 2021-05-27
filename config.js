11da0196a6cc7daec233c89525ea95690d0dce33
Josiah Swanson <josswanson@westmont.edu>
2021-05-27 12:13:14 -1000

    Localhost config

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ exports.creds = {
     responseMode: 'form_post', 
   
     // Required, the reply URL registered in AAD for your app
-    redirectUrl: 'https://hunt-swanson.westus2.azurecontainer.io:8080/auth/openid/return', 
+    redirectUrl: 'http://localhost:8080/auth/openid/return', 
   
     // Required if we use http for redirectUrl
     allowHttpForRedirectUrl: true,
@@ -70,7 +70,7 @@ exports.creds = {
   };
   
   // The url you need to go to destroy the session with AAD
-  exports.destroySessionUrl = 'https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=https://hunt-swanson.westus2.azurecontainer.io:8080';
+  exports.destroySessionUrl = 'https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=https://localhost:8080';
   
   // If you want to use the mongoDB session store for session middleware, set to true; otherwise we will use the default
   // session store provided by express-session.
